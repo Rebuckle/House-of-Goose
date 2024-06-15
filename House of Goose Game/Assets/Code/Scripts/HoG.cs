917ee6422605f7437d9938ea379d56bb1cb95207
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-06-15 00:16:24 -0400

    ScriptableObjects Backend, Updated SO File Structure

--- House of Goose Game/Assets/Code/Scripts/HoG.cs
+++ House of Goose Game/Assets/Code/Scripts/HoG.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 /// Base Monobehavior class for the House of Goose game to define common or static variables. Important Game Objects can inheret from this class to inheret Monobhevaior and associated object types.
 /// </summary>
 public class HoG : MonoBehaviour
-{    
+{
     /// <summary>
     /// The primary seasons that a trip can be scheduled for. Also used to define Peak and Down season for each location.
     /// </summary>
@@ -14,4 +14,8 @@ public class HoG : MonoBehaviour
     /// Defines the primary regions that locations and clients can be based in for the purpose of determining flight needs.
     /// </summary>
     public enum Regions { NorthAmerica, SouthAmerica, Caribbean, Europe, Asia, Africa, Oceania };
+
+    public enum Attributes { old, modern, open, beach, dense };
+
+    public enum Locations { Edinburgh, Paris, Busan, Tokyo, Delhi, Bali, Portland, Santorini };
 }
--- House of Goose Game/Assets/Code/Scripts/IExperience.cs
+++ House of Goose Game/Assets/Code/Scripts/IExperience.cs
@@ -6,59 +6,27 @@ public interface IExperience
     /// <summary>
     /// The types of experiences that can be entered into the Itinerary.
     /// </summary>
-    enum ExperienceType { Dining, Transit, Lodging, Activity };
+    public enum ExperienceEnum { Dining, Transit, Lodging, Activity };
 
-    /// <summary>
-    /// Returns key information about the particular IExperience, which may include Name, Quality, and Cost.
-    /// </summary>
-    /// <returns>Returns an IExperience object type.</returns>
-    public IExperience ReturnExperience();
-    /// <summary>
-    /// Function to create a new Experience Game Object using standard types.
-    /// </summary>
-    /// <param name="_name">Describes the name of the Experience.</param>
-    /// <param name="_quality">Quantifies the sentimental value of the Experience.</param>
-    /// <param name="_cost">Quantifies the monetary value of the Experience.</param>
-    /// <param name="experienceType">Qualifies the type of Experience.</param>
-    public void AddExperience(string _name, int _quality, double _cost, ExperienceType experienceType);
-    /// <summary>
-    /// The name of the Experience.
-    /// </summary>
-    /// <returns>String value of Experience.</returns>
-    public string GetName();
-    /// <summary>
-    /// Assigns a name to the Experience.
-    /// </summary>
-    /// <param name="_name"></param>
-    public void SetName(string _name);
-    /// <summary>
-    /// The name of the Experience.
-    /// </summary>
-    /// <returns>String value of Experience.</returns>
-    public int GetQuality();
-    /// <summary>
-    /// Assigns an integer value for the quality of the Experience.
-    /// </summary>
-    /// <param name="_quality"></param>
-    public void SetQuality(int _quality);
-    /// <summary>
-    /// The quality of the Experience.
-    /// </summary>
-    /// <returns>Integer value of Experience.</returns>
-    public double GetCost();
-    /// <summary>
-    /// Assigns a double value for the cost of the Experience.
-    /// </summary>
-    /// <param name="_cost"></param>
-    public void SetCost(double _cost);
-    /// <summary>
-    /// The type of the Experience.
-    /// </summary>
-    /// <returns>ExperienceType value of Experience.</returns>
-    public ExperienceType GetExperience();
-    /// <summary>
-    /// Assigns a type for the Experience.
-    /// </summary>
-    /// <param name="_experienceType"></param>
-    public void SetExperienceType(ExperienceType _experienceType);
+    public ExperienceEnum ExperienceType {
+        get;
+    }
+
+    public string Name
+    {
+        get;
+        set;
+    }
+
+    public int Quality
+    {
+        get;
+        set;
+    }
+
+    public double Cost
+    {
+        get;
+        set;
+    }
 }
