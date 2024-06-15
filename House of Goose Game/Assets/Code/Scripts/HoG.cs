5b6b5f9033e70e10c2a457967479447db4329eae
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-06-14 21:18:52 -0400

    Updated enumerators in HoG to be Public

--- House of Goose Game/Assets/Code/Scripts/HoG.cs
+++ House of Goose Game/Assets/Code/Scripts/HoG.cs
@@ -8,10 +8,10 @@ public class HoG : MonoBehaviour
     /// <summary>
     /// The primary seasons that a trip can be scheduled for. Also used to define Peak and Down season for each location.
     /// </summary>
-    enum Seasons { Winter, Spring, Summer, Fall };
+    public enum Seasons { Winter, Spring, Summer, Fall };
 
     /// <summary>
     /// Defines the primary regions that locations and clients can be based in for the purpose of determining flight needs.
     /// </summary>
-    enum Regions { NorthAmerica, SouthAmerica, Caribbean, Europe, Asia, Africa, Oceania };
+    public enum Regions { NorthAmerica, SouthAmerica, Caribbean, Europe, Asia, Africa, Oceania };
 }
