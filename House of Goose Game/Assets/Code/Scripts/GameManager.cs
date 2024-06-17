0f0bbac45e86f977f7b462e4238f9f9bed8f983d
Darya Talia <92874279+DaryaTalia@users.noreply.github.com>
2024-06-17 00:22:55 -0400

    Functions for Displaying Notebook UI
    Issue: Page UI disappears in play mode
    Next Steps: Connect all notebook data to Current Client object

---
+++ House of Goose Game/Assets/Code/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GameManager : HoG
+{
+    public static GameManager gm;
+
+    public Client currentClient;
+
+    //Feature Behaviors
+    public NotebookBehavior notebookBehavior;
+    public LocationBehavior locationBehavior;
+
+    private void Awake()
+    {
+        if(gm != null && gm != this)
+        {
+            Destroy(this);
+        } else
+        {
+            gm = this;
+        }
+
+        if(notebookBehavior && locationBehavior)
+        {
+            notebookBehavior.locationBehavior = locationBehavior;
+        }
+    }//end Awake
+
+
+}
