0a423f905bc9127cd031ed4126a380a49e0a01e2
delton <hulbert.delton@gmail.com>
2024-06-19 05:11:55 -0400

    preventing duplicate adds

--- House of Goose Game/Assets/Code/Scripts/EventHandling/ObjectSubscriptions/EventsManilla.cs
+++ House of Goose Game/Assets/Code/Scripts/EventHandling/ObjectSubscriptions/EventsManilla.cs
@@ -6,6 +6,14 @@ public class EventsManilla : ObjectEventSubscriber
 {
     protected override void OnGameStart(object gameManager, GameStartArgs gsa)
     {
-        gsa.manillas.Add(transform);
+        base.OnGameStart(gameManager, gsa);
+        if (gsa.manillas == null) {
+            gsa.manillas = new();
+        }
+
+        if(!gsa.manillas.Contains(transform))
+        {
+            gsa.manillas.Add(transform);
+        }
     }
 }
