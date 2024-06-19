07f87807c3b299d810c7bb64616843a504583803
delton <hulbert.delton@gmail.com>
2024-06-19 09:57:50 -0400

    updating manilla and sfx

--- House of Goose Game/Assets/Code/Scripts/EventHandling/ObjectSubscriptions/EventsManilla.cs
+++ House of Goose Game/Assets/Code/Scripts/EventHandling/ObjectSubscriptions/EventsManilla.cs
@@ -1,5 +1,22 @@
+using UnityEngine;
+
 public class EventsManilla : ObjectEventSubscriber
 {
+    [SerializeField] private Texture locationImageImage;
+    [SerializeField] private Texture locationDataImage;
+    [SerializeField] private GameObject locationImageGO;
+    [SerializeField] private GameObject locationDataGO;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        Material imgmat = locationImageGO.GetComponent<Material>();
+        imgmat.SetTexture("Base", locationImageImage);
+        Material datamat = locationDataGO.GetComponent<Material>();
+        datamat.SetTexture("Base", locationDataImage);
+
+    }
+
     protected override void OnGameStart(object gameManager, GameStartArgs gsa)
     {
         base.OnGameStart(gameManager, gsa);
